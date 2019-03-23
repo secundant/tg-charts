946ad38c54952bf415aa7743a3047bf71cacb4a4
Dmitry Remezov <mysecundant@gmail.com>
2019-03-23 23:47:45 +0300

    fix: Replace @now/node with @now/node-server

--- index.server.js
+++ index.server.js
@@ -2,6 +2,7 @@ const Koa = require('koa');
 const serve = require('koa-static');
 const compress = require('koa-compress');
 const applicaion = new Koa();
+const port = process.env.PORT || 80;
 
 applicaion.use(compress({
   threshold: 2048,
@@ -9,10 +10,10 @@ applicaion.use(compress({
 }));
 applicaion.use(serve('static'));
 
-applicaion.listen(80, error => {
+applicaion.listen(port, error => {
   if (error) {
     console.error(error);
     process.exit(1);
   }
-  console.log('Server is ready');
+  console.log(`Server is ready on ${port} port`);
 });
